79102164996d7174b20affab5255addae06c3b3e
mikoz <vasco.ozdowski829@gmail.com>
2018-11-14 20:30:59 +0100

    14-11-2018

---
+++ oop/cwiczenie_4.py
@@ -0,0 +1,16 @@
+from cwiczenie_1 import Product
+
+class Basket():
+
+    def __init__(self):
+        pass
+
+    def add_product(self, product, qty):
+        self.productt = product
+        self.qty = qty
+
+    def count_total_price(self):
+
+
+    def generate_report(self):
+        pass
--- oop/definiowanie_klas.py
+++ oop/definiowanie_klas.py
@@ -1,16 +1,15 @@
 
-
-class Animal:
-    krolestwo = "Fauna"
-
-    def _init_(self, nazwa):
-        self.nazwa = nazwa
-
-a1 = Animal("Żyrafa") #instancja klasy
-a2 = Animal("Mysz") #instancja klasy
-
-print(a1.nazwa)
-print(a2.nazwa)
+# class Animal:
+#     krolestwo = "Fauna"
+#
+#     def _init_(self, nazwa):
+#         self.nazwa = nazwa
+#
+# a1 = Animal("Żyrafa") #instancja klasy
+# a2 = Animal("Mysz") #instancja klasy
+#
+# print(a1.nazwa)
+# print(a2.nazwa)
 
 # print(type(zyrafa))
 #
@@ -28,3 +27,24 @@ print(a2.nazwa)
 # zyrafa.krolestwo = "Fauna"
 #
 # print(zyrafa.krolestwo)
+class Osoba():
+
+    def __init__(self, imie, nazwisko):
+        print("No siema")
+        self.imie = imie
+        self.nazwisko = nazwisko
+
+
+    def przedstaw_sie(self):
+        print(f"Nazywam się {self.imie} {self.nazwisko}")
+
+    @staticmethod
+    def metoda_statyczna():
+        print("Metoda statyczna")
+
+obiekt = Osoba("Adam", "Małysz")
+obiekt2 = Osoba("Adam", "Mickiewicz")
+
+obiekt.przedstaw_sie()
+obiekt2.przedstaw_sie()
+Osoba.metoda_statyczna()
